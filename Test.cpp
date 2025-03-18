21add58bec24d6afad046b59ea2f129c0d16b463
cdac <cdacatcnashik@gmail.com>
2025-03-18 15:43:28 +0530

    two features added

---
+++ Test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"code running";
+    return 0;
+}
---
+++ util/utils.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void add(int x, int y){
+    cout << x+y;
+}
+
+int main(){
+    add(10, 20);
+    return 0;
+}
