268966edee304eb89eeb2b1e7e8b9c4accaad147
cdac <cdacatcnashik@gmail.com>
2025-03-18 15:52:15 +0530

    added isAdult fn

--- Demo.cpp
+++ Demo.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void isAdult(int age){
+    if(age >= 18){
+        cout << "Adult";
+    }else{
+        cout << "Minor";
+    }
+}
+
 int main(){
     cout<<"Hello from git";
+    int age = 20;
+    int name = "Jack";
+    isAdult(age);
     return 0;
 }
